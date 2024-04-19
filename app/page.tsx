3a49119973c22d89268deb8987c478ed8791cbf6
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-19 15:13:24 -0700

    Get sessions from airtable

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { getSessions } from "@/db/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const sessions = await getSessions();
+  console.log(sessions);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
--- db/db.ts
+++ db/db.ts
@@ -4,3 +4,27 @@ Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
 });
 var base = Airtable.base("appklVAIrAhkGj98d");
+
+export async function getSessions() {
+  const sessions: any[] = [];
+  await base("Sessions")
+    .select({
+      fields: [
+        "Title",
+        "Description",
+        "Start time",
+        "End time",
+        "Hosts",
+        "Host name",
+        "Host email",
+        "Location",
+      ],
+    })
+    .eachPage(function page(records: any, fetchNextPage: any) {
+      records.forEach(function (record: any) {
+        sessions.push(record.fields);
+      });
+      fetchNextPage();
+    });
+  return sessions;
+}
