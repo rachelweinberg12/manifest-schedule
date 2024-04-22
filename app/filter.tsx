cee0ccb874c28ae097162f17159152e5d4ac4e57
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-22 15:09:35 -0700

    Add location category selection

--- app/filter.tsx
+++ app/filter.tsx
@@ -15,75 +15,95 @@ export function Filter(props: { locations: Location[] }) {
   const { locations } = props;
   const searchParams = useSearchParams();
   return (
-    <SelectLocationsToShow locations={locations} searchParams={searchParams} />
+    <div className="flex flex-col gap-4">
+      <SelectLocCategoryToShow
+        locations={locations}
+        searchParams={searchParams}
+      />
+      <SelectLocationsToShow
+        locations={locations}
+        searchParams={searchParams}
+      />
+    </div>
   );
 }
 
 // TODO: store in airtable
 const MAIN_SESSION_SPACES = ["Rat Park", "1E Main"];
-// function SelectSessionsToShow(props: { locations: Location[] }) {
-//   const { locations } = props;
-//   const sessionCategories = [
-//     { name: "All", locations: locations },
-//     {
-//       name: "Main",
-//       locations: locations.filter((loc) =>
-//         MAIN_SESSION_SPACES.includes(loc.Name)
-//       ),
-//     },
-//     {
-//       name: "Side",
-//       locations: locations.filter(
-//         (loc) => !MAIN_SESSION_SPACES.includes(loc.Name)
-//       ),
-//     },
-//   ];
-//   return (
-//     <div>
-//     <div className="sm:hidden">
-//       <label htmlFor="tabs" className="sr-only">
-//         Select a tab
-//       </label>
-//       {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-//       <select
-//         id="tabs"
-//         name="tabs"
-//         className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-//         defaultValue={sessionCategories.find((tab) => tab.current).name}
-//       >
-//         {sessionCategories.map((tab) => (
-//           <option key={tab.name}>{tab.name}</option>
-//         ))}
-//       </select>
-//     </div>
-//     <div className="hidden sm:block">
-//       <nav className="flex space-x-4" aria-label="sessionCategories">
-//         {sessionCategories.map((tab) => (
-//           <a
-//             key={tab.name}
-//             href={tab.href}
-//             className={classNames(
-//               tab.current
-//                 ? "bg-indigo-100 text-indigo-700"
-//                 : "text-gray-500 hover:text-gray-700",
-//               "rounded-md px-3 py-2 text-sm font-medium"
-//             )}
-//             aria-current={tab.current ? "page" : undefined}
-//           >
-//             {tab.name}
-//           </a>
-//         ))}
-//       </nav>
-//     </div>
-//   </div>
-//   )
-// }
+function SelectLocCategoryToShow(props: {
+  locations: Location[];
+  searchParams: ReadonlyURLSearchParams;
+}) {
+  const { locations, searchParams } = props;
+  const locCategories = [
+    { name: "all", locations: locations },
+    {
+      name: "main",
+      locations: locations.filter((loc) =>
+        MAIN_SESSION_SPACES.includes(loc.Name)
+      ),
+    },
+    {
+      name: "side",
+      locations: locations.filter(
+        (loc) => !MAIN_SESSION_SPACES.includes(loc.Name)
+      ),
+    },
+  ];
+  const currentLocCategory = locCategories.find(
+    (category) => category.name === props.searchParams.get("locCategory")
+  );
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const urlSearchParams = new URLSearchParams(searchParams);
+  return (
+    <div>
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a tab
+        </label>
+        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full rounded-md border-gray-300 focus:border-gray-500 focus:ring-gray-500"
+          defaultValue={locCategories[0].name}
+        >
+          {locCategories.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <nav className="flex space-x-4" aria-label="locCategories">
+          {locCategories.map((tab) => (
+            <button
+              key={tab.name}
+              onClick={() => {
+                urlSearchParams.set("locCategory", tab.name);
+                replace(`${pathname}?${urlSearchParams.toString()}`);
+              }}
+              className={clsx(
+                currentLocCategory === tab
+                  ? "bg-gray-100 text-gray-700"
+                  : "text-gray-500 hover:text-gray-700",
+                "rounded-md px-3 py-2 text-sm font-medium"
+              )}
+            >
+              {tab.name}
+            </button>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+}
 
 function SelectLocationsToShow(props: {
   locations: Location[];
   searchParams: ReadonlyURLSearchParams;
 }) {
-  const { searchParams } = props;
+  const { locations, searchParams } = props;
   const urlSearchParams = new URLSearchParams(searchParams);
   const [includedLocations, setIncludedLocations] = useState(
     urlSearchParams.getAll("loc")
