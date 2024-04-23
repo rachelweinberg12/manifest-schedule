1b22c718a81c8657427f2968e5484135fd0b53c0
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-23 15:32:43 -0700

    Change from popover to tooltip

--- app/session.tsx
+++ app/session.tsx
@@ -5,6 +5,7 @@ import { Popover } from "@headlessui/react";
 import { ClockIcon, UserIcon } from "@heroicons/react/24/outline";
 import { format } from "date-fns";
 import { Location } from "@/utils/db";
+import { Tooltip } from "./tooltip";
 
 export function SessionCard(props: { session: Session; location: Location }) {
   const { session, location } = props;
@@ -15,46 +16,50 @@ export function SessionCard(props: { session: Session; location: Location }) {
   const formattedHostNames = session["Host name"].join(", ");
   const isBlank = session.Title === "";
   const isMain = location.Type === "main";
+  const TooltipContents = () => (
+    <>
+      <h1 className="text-lg font-bold">{session.Title}</h1>
+      <p className="text-xs text-gray-500 mb-2">{formattedHostNames}</p>
+      <p className="text-sm">{session.Description}</p>
+      <div className="flex justify-between mt-2 text-xs text-gray-500">
+        <div className="flex gap-1">
+          <UserIcon className="h-4 w-4" />
+          <span>60</span>
+        </div>
+        <div className="flex gap-1">
+          <ClockIcon className="h-4 w-4" />
+          <span>
+            {format(new Date(session["Start time"]), "h:mm")} -{" "}
+            {format(new Date(session["End time"]), "h:mm")}
+          </span>
+        </div>
+      </div>
+    </>
+  );
   return (
-    <Popover
-      className={clsx(
-        "py-1 px-1.5 my-0.5 rounded font-roboto w-full flex relative",
-        `row-span-${numHalfHours}`,
-        isMain && !isBlank
-          ? `bg-${
-              locationColors[session["Location name"][0]]
-            }-200 border-2 border-${
-              locationColors[session["Location name"][0]]
-            }-400`
-          : `bg-${locationColors[session["Location name"][0]]}-200`
-      )}
+    <Tooltip
+      content={<TooltipContents />}
+      className={`row-span-${numHalfHours} my-0.5`}
     >
-      <Popover.Button className="w-full h-full focus:outline-0 flex flex-col justify-start">
+      <div
+        className={clsx(
+          "py-1 px-1.5 rounded font-roboto h-full",
+          isMain && !isBlank
+            ? `bg-${
+                locationColors[session["Location name"][0]]
+              }-200 border-2 border-${
+                locationColors[session["Location name"][0]]
+              }-400`
+            : `bg-${locationColors[session["Location name"][0]]}-200`
+        )}
+      >
         <p className="font-medium text-xs leading-tight line-clamp-2 text-left">
           {session.Title}
         </p>
         <p className="text-[10px] leading-tight text-left">
           {formattedHostNames}
         </p>
-      </Popover.Button>
-      <Popover.Panel className="absolute bottom-5 left-5 rounded-md p-3 shadow-md z-10 bg-white border border-gray-100 font-monteserrat w-80">
-        <h1 className="text-lg font-bold">{session.Title}</h1>
-        <p className="text-xs text-gray-500 mb-2">{formattedHostNames}</p>
-        <p className="text-sm">{session.Description}</p>
-        <div className="flex justify-between mt-2 text-xs text-gray-500">
-          <div className="flex gap-1">
-            <UserIcon className="h-4 w-4" />
-            <span>60</span>
-          </div>
-          <div className="flex gap-1">
-            <ClockIcon className="h-4 w-4" />
-            <span>
-              {format(new Date(session["Start time"]), "h:mm")} -{" "}
-              {format(new Date(session["End time"]), "h:mm")}
-            </span>
-          </div>
-        </div>
-      </Popover.Panel>
-    </Popover>
+      </div>
+    </Tooltip>
   );
 }
---
+++ app/tooltip.tsx
@@ -0,0 +1,118 @@
+"use client";
+import {
+  arrow,
+  autoUpdate,
+  flip,
+  offset,
+  Placement,
+  safePolygon,
+  shift,
+  useFloating,
+  useHover,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react";
+import { Transition } from "@headlessui/react";
+import { ReactNode, useRef, useState } from "react";
+
+// See https://floating-ui.com/docs/react-dom
+
+export function Tooltip(props: {
+  content: ReactNode;
+  children: ReactNode;
+  className?: string;
+  placement?: Placement;
+  noTap?: boolean;
+  noFade?: boolean;
+  hasSafePolygon?: boolean;
+  suppressHydrationWarning?: boolean;
+}) {
+  const {
+    content,
+    children,
+    className,
+    noTap,
+    noFade,
+    hasSafePolygon,
+    suppressHydrationWarning,
+  } = props;
+
+  const arrowRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
+
+  const { x, y, refs, strategy, middlewareData, context, placement } =
+    useFloating({
+      open: open,
+      onOpenChange: setOpen,
+      whileElementsMounted: autoUpdate,
+      placement: props.placement ?? "top",
+      middleware: [
+        offset(8),
+        flip(),
+        shift({ padding: 4 }),
+        arrow({ element: arrowRef }),
+      ],
+    });
+
+  const { x: arrowX, y: arrowY } = middlewareData.arrow ?? {};
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    useHover(context, {
+      mouseOnly: noTap,
+      handleClose: hasSafePolygon ? safePolygon({ buffer: -0.5 }) : null,
+    }),
+    useRole(context, { role: "tooltip" }),
+  ]);
+  // which side of tooltip arrow is on. like: if tooltip is top-left, arrow is on bottom of tooltip
+  const arrowSide = {
+    top: "bottom",
+    right: "left",
+    bottom: "top",
+    left: "right",
+  }[placement.split("-")[0]] as string;
+
+  return content ? (
+    <div className={className}>
+      <span
+        suppressHydrationWarning={suppressHydrationWarning}
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
+        {children}
+      </span>
+      {/* conditionally render tooltip and fade in/out */}
+      <Transition
+        show={open}
+        enter="transition ease-out duration-50"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave={noFade ? "" : "transition ease-in duration-150"}
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        // div attributes
+        role="tooltip"
+        ref={refs.setFloating}
+        style={{ position: strategy, top: y ?? 0, left: x ?? 0 }}
+        className="z-20 w-max max-w-lg whitespace-normal rounded bg-white px-2 py-1 border border-gray-100"
+        suppressHydrationWarning={suppressHydrationWarning}
+        {...getFloatingProps()}
+      >
+        {content}
+        <div
+          ref={arrowRef}
+          className="absolute h-2 w-2 rotate-45 bg-white"
+          style={{
+            top: arrowY != null ? arrowY : "",
+            left: arrowX != null ? arrowX : "",
+            right: "",
+            bottom: "",
+            [arrowSide]: "-4px",
+          }}
+        />
+      </Transition>
+    </div>
+  ) : (
+    <div className={className}>{children}</div>
+  );
+}
