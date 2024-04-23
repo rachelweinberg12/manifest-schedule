24191427630a909df1cc50a52a6c6f8647593fa2
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-04-23 12:13:28 -0700

    Fix breakpoint styling on pagination control

--- app/day.tsx
+++ app/day.tsx
@@ -29,27 +29,34 @@ export function DayCol(props: {
   const displayedLocations = includedLocations.slice(0, numColsToDisplay);
   return (
     <div className="w-full">
-      <div className="flex items-end justify-between mb-5">
-        <h2 className="text-3xl font-bold">{format(start, "EEEE, MMMM d")}</h2>
-        <div className="flex items-center gap-3">
-          <span className="text-gray-500 text-xs">
-            Showing locations 1-3 of 6
-          </span>
-          <span className="isolate inline-flex rounded-md shadow-sm">
-            <button
-              type="button"
-              className="relative inline-flex items-center rounded-l-md bg-white px-1.5 py-1.5 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-            >
-              <ChevronLeftIcon className="h-4 w-4" aria-hidden="true" />
-            </button>
-            <button
-              type="button"
-              className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-1.5 py-1.5 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-            >
-              <ChevronRightIcon className="h-4 w-4" aria-hidden="true" />
-            </button>
-          </span>
+      <div className="flex flex-col mb-5">
+        <div className="flex items-center justify-between">
+          <h2 className="text-2xl font-bold">
+            {format(start, "EEEE, MMMM d")}
+          </h2>
+          <div className="flex items-center gap-3">
+            <span className="text-gray-500 text-xs hidden sm:block">
+              Showing locations 1-3 of 6
+            </span>
+            <span className="isolate inline-flex rounded-md shadow-sm">
+              <button
+                type="button"
+                className="relative inline-flex items-center rounded-l-md bg-white px-1.5 py-1.5 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+              >
+                <ChevronLeftIcon className="h-4 w-4" aria-hidden="true" />
+              </button>
+              <button
+                type="button"
+                className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-1.5 py-1.5 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+              >
+                <ChevronRightIcon className="h-4 w-4" aria-hidden="true" />
+              </button>
+            </span>
+          </div>
         </div>
+        <span className="text-gray-500 text-xs sm:hidden text-right mt-1">
+          Showing locations 1-3 of 6
+        </span>
       </div>
       <div
         className={clsx(
