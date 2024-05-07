c67365a40ed101789b2bbfb52aeb2e3a0e8d565c
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-05-07 16:44:20 -0600

    Change location to dropdown

--- app/add-session/add-session-form.tsx
+++ app/add-session/add-session-form.tsx
@@ -22,7 +22,7 @@ export function AddSessionForm(props: {
   const [startTime, setStartTime] = useState("");
   const [duration, setDuration] = useState(30);
   const [hosts, setHosts] = useState<Guest[]>([]);
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState<Location>();
   const availableStartTimes = getAvailableStartTimes(day, sessions, location);
   console.log(availableStartTimes);
   const DURATIONS = [
@@ -162,7 +162,61 @@ export function AddSessionForm(props: {
       </div>
       <div className="flex flex-col gap-1">
         <label>Location</label>
-        <Input value={location} onChange={(e) => setLocation(e.target.value)} />
+        <Listbox value={location} onChange={setLocation}>
+          <div className="relative mt-1">
+            <Listbox.Button className="h-12 rounded-md border px-4 shadow-sm transition-colors invalid:border-red-500 invalid:text-red-900 focus:outline-none relative w-full cursor-pointer border-gray-300 focus:ring-2 focus:ring-rose-400 focus:outline-0 focus:border-none bg-white py-2 pl-3 pr-10 text-left">
+              {location ? (
+                <span className="block truncate">{location.Name}</span>
+              ) : (
+                <span className="block truncate text-gray-400">
+                  Select a location
+                </span>
+              )}
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute mt-1 max-h-60 w-72 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                {locations.map((loc) => {
+                  return (
+                    <Listbox.Option
+                      key={loc.Name}
+                      value={loc}
+                      className={({ active }) =>
+                        `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-rose-100 text-rose-900" : "text-gray-900"
+                        }`
+                      }
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {loc["Name"]}
+                          </span>
+                          {selected ? (
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-rose-400">
+                              <CheckIcon className="h-5 w-5" />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  );
+                })}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
       </div>
       <button
         type="submit"
@@ -180,12 +234,14 @@ export function AddSessionForm(props: {
 function getAvailableStartTimes(
   day: Day,
   sessions: Session[],
-  location: string
+  location?: Location
 ) {
   const takenStartTimes = sessions
     .filter(
       (session) =>
-        session["Location name"] && session["Location name"][0] === location
+        session["Location name"] &&
+        location &&
+        session["Location name"][0] === location.Name
     )
     .map((session) => new Date(session["Start time"]).getTime());
   const availableStartTimes = [];
