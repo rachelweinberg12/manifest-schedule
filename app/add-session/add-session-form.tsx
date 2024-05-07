d245d91177543749771bb0c8cacbecba17195d8e
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-05-07 09:59:38 -0600

    Style time select

--- app/add-session/add-session-form.tsx
+++ app/add-session/add-session-form.tsx
@@ -74,7 +74,9 @@ export function AddSessionForm(props: {
         <Listbox value={startTime} onChange={setStartTime}>
           <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-pointer rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-rose-400 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-rose-400 sm:text-sm">
-              <span className="block truncate">{startTime}</span>
+              <span className="block truncate">
+                {format(startTime, "h:mm a")}
+              </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon
                   className="h-5 w-5 text-gray-400"
@@ -89,35 +91,38 @@ export function AddSessionForm(props: {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute mt-1 max-h-60 w-72 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                {availableStartTimes.map((startTime) => (
-                  <Listbox.Option
-                    key={startTime}
-                    value={startTime}
-                    // disabled={startTime}
-                    className={({ active }) =>
-                      `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-rose-100 text-rose-900" : "text-gray-900"
-                      }`
-                    }
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {startTime}
-                        </span>
-                        {selected ? (
-                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-rose-400">
-                            <CheckIcon className="h-5 w-5" />
+                {availableStartTimes.map((startTime) => {
+                  const formattedStartTime = format(startTime, "h:mm a");
+                  return (
+                    <Listbox.Option
+                      key={startTime}
+                      value={startTime}
+                      // disabled={startTime}
+                      className={({ active }) =>
+                        `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-rose-100 text-rose-900" : "text-gray-900"
+                        }`
+                      }
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {formattedStartTime}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
+                          {selected ? (
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-rose-400">
+                              <CheckIcon className="h-5 w-5" />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  );
+                })}
               </Listbox.Options>
             </Transition>
           </div>
