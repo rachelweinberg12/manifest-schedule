db1162fac8a31f66c9893de5542bd92507fc8441
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-05-10 19:21:50 -0600

    Move add session form into route with event slug

--- app/event.tsx
+++ app/event.tsx
@@ -13,7 +13,7 @@ export function Event(props: { days: Day[]; locations: Location[] }) {
     <div className="flex flex-col gap-5 items-start w-full">
       <h1 className="text-3xl font-bold text-center">{eventName}</h1>
       <Filter locations={locations} />
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-24 mt-12">
         {daysForEvent.map((day) => (
           <DayCol key={day.Start} day={day} locations={locations} />
         ))}
