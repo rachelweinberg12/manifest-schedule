a88be53493a1e15b7fd83fc0fe7361958183eea1
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-05-13 16:48:37 -0600

    Add outlines to all events

--- app/[eventSlug]/session.tsx
+++ app/[eventSlug]/session.tsx
@@ -132,7 +132,7 @@ export function RealSessionCard(props: {
       <div
         className={clsx(
           "py-1 px-1.5 rounded font-roboto h-full",
-          `bg-${location.Color}-200`
+          `bg-${location.Color}-200 border-2 border-${location.Color}-400`
         )}
       >
         <p className="font-medium text-xs leading-tight line-clamp-2 text-left">
